6b5f64e008a2a9cfce087b8df1202ac5354c4f6e
Tristan Holl <mail@tristanholl.com>
2024-10-18 14:06:05 +0200

    finalize aggregate

--- src/components/aggregate.cr
+++ src/components/aggregate.cr
@@ -4,14 +4,11 @@ module ES
     abstract struct State
       # Properties of the aggregate
       getter aggregate_id : UUID
-      getter aggregate_type : String
-      getter aggregate_version : Int32
+      getter aggregate_type : String = "undefined"
+      getter aggregate_version : Int32 = 0
 
       # Initialize the state of the aggregate
-      def initialize(@aggregate_id)
-        @aggregate_version = 0
-        @aggregate_type = "undefined"
-      end
+      def initialize(@aggregate_id); end
 
       # Increase the current version of the aggregate without applying any business logic
       def increase_version(version : Int32)
@@ -57,12 +54,36 @@ module ES
     )
     end
 
+    # Handle events up to a certain version
+    def apply(
+      event : ES::EventStore::Event,
+      up_to_version : Int32
+    )
+      h = ES::Event::Header.from_json(event.header.to_json)
+      return if h.aggregate_version > up_to_version
+
+      apply(@event_handlers.event_class(h.event_handle).new(h, event.body))
+    end
+
     # Applying an unspecified event to the aggregate
     def apply(event : ES::Event)
       if @reject_unhandled_events
         raise ES::Exception::InvalidEventStream.new("Event not handled: '#{event.class}' in aggregate '#{event.header.aggregate_type}'")
       else
         @state.increase_version(event.header.aggregate_version)
+      end
+    end
+
+    # Hydrate the aggregate state from events
+    def hydrate(version : Int32 = Int32::MAX)
+      es = @event_store
+      raise ES::Exception::DependencyUnavailable.new("Eventstore not available") if es.nil?
+
+      events = es.fetch_events(@state.aggregate_id)
+      raise ES::Exception::NotFound.new("Aggregate '#{@state.aggregate_id}' of type '#{@state.aggregate_type}' not found") if events.empty?
+
+      events.each do |event|
+        apply(event, up_to_version: version)
       end
     end
   end
