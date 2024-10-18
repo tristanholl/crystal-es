058f9a19c52cf03a124052b987c8b1cc45112df0
Tristan Holl <mail@tristanholl.com>
2024-10-18 16:41:49 +0200

    Implement first projection

--- src/components/projection.cr
+++ src/components/projection.cr
@@ -1,33 +1,33 @@
 # TODO: Implement projector
 module ES
   abstract class Projection
-    # @projection_db : DB::Database
-    # @event_store : ES::EventStore
-    # @event_handlers : ES::EventHandlers
+    @event_handlers : ES::EventHandlers
+    @event_store : ES::EventStore
+    @projection_database : DB::Database
 
-    # def initialize(
-    #   @projection_db : DB::Database = ES::Config.projection_db,
-    #   @event_store : ES::EventStore = ES::Config.event_store,
-    #   @event_handlers : ES::EventHandlers = ES::Config.event_handlers
-    # )
-    #   @event_id = UUID.v7
-    # end
+    def initialize(
+      @event_handlers : ES::EventHandlers = ES::Config.event_handlers,
+      @event_store : ES::EventStore = ES::Config.event_store,
+      @projection_database : DB::Database = ES::Config.projection_database
+    )
+      @event_id = UUID.v7
+    end
 
-    # def initialize(
-    #   @event_id : UUID,
-    #   @projection_db : DB::Database = ES::Config.projection_db,
-    #   @event_store : ES::EventStore = ES::Config.event_store,
-    #   @event_handlers : ES::EventHandlers = ES::Config.event_handlers
-    # )
-    # end
+    def initialize(
+      @event_id : UUID,
+      @event_handlers : ES::EventHandlers = ES::Config.event_handlers,
+      @event_store : ES::EventStore = ES::Config.event_store,
+      @projection_database : DB::Database = ES::Config.projection_database
+    )
+    end
 
-    # def call(event : ES::Event)
-    #   @event_id = event.header.event_id
-    #   apply(event)
-    # end
+    def call(event : ES::Event)
+      @event_id = event.header.event_id
+      apply(event)
+    end
 
-    # # This method catches all unhandled events
-    # protected def apply(event : ES::Event)
-    # end
+    # This method catches all unhandled events
+    protected def apply(event : ES::Event)
+    end
   end
 end
